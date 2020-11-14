49ac017541a0fd27615cdfac0fda412c8d3006e4
Felandil <s.fuhrhop@hotmail.de>
2020-11-14 22:28:35 +0100

    add test project

--- Clients/gp-client/src/App.js
+++ Clients/gp-client/src/App.js
@@ -6,10 +6,11 @@ import { setupApp } from '../../shared/utils/utils';
 class App extends Component {
   constructor(props) {
     super(props);
-    setupApp('210f24c9cb5d49478cfc265def05626f');
     this.state = {
     	messages: null,
     }
+    
+    setupApp('210f24c9cb5d49478cfc265def05626f');
   }
 
   render() {
