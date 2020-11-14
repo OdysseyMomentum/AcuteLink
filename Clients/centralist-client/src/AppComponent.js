86384bbd9121ed0140d97ef9ea54e5e6c451fa55
Felandil <s.fuhrhop@hotmail.de>
2020-11-14 22:20:40 +0100

    add breadcrumb nav

--- Clients/centralist-client/src/AppComponent.js
+++ Clients/centralist-client/src/AppComponent.js
@@ -2,6 +2,7 @@ import { Navbar } from '../../shared/components/navbar';
 import { Card } from '../../shared/components/card';
 import { Dependencies } from '../../shared/dependencies';
 import { AppBottom } from '../../shared/components/appBottom';
+import { BreadcrumbNavigation } from '../../shared/components/breadcrumb';
 
 export const AppComponent = ({messages}) => {
     return (
@@ -18,12 +19,7 @@ export const AppComponent = ({messages}) => {
                         </div>
                     </nav>
                     <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
-                        <nav aria-label="breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Overview</li>
-                            </ol>
-                        </nav>
+                        <BreadcrumbNavigation />
                         <h1 class="h2">Dashboard</h1>
                         <p>This is the homepage of a simple centralist interface...</p>
                         <div class="row">
---
+++ Clients/shared/components/breadcrumb.js
@@ -0,0 +1,10 @@
+export const BreadcrumbNavigation = () => {
+    return(
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Overview</li>
+            </ol>
+        </nav>
+    )
+}
